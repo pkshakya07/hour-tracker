0bbc06a5ddb32de463742bdff8d6ccfb1a07131f
Pawan <pawankumar.shakya@uni-hohenheim.de>
2026-05-09 13:55:50 +0200

    Force service worker update v4

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'worktrack-v3';
+const CACHE_NAME = 'worktrack-v4';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting(); // Forces the new service worker to take over immediately
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
